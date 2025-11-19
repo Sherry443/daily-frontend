b316e7573843ac610d7ba4b9d3a6795df76070bd
Sherry443 <shahzadrasool443@gmail.com>
2025-11-20 00:11:17 +0500

    urls changes

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const BACKEND_URL = 'https://daily-backend-zeta.vercel.app/';
+const BACKEND_URL = 'https://daily-backend-riders.onrender.com';
 
 // Login Component
 function Login({ onLogin }) {
